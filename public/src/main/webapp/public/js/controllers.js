2de331f1e7362ea83128ea9c38973f11320f8a4e
unknown <lhajdu@HUB-LHAJDU-W7.mgc.mentorg.com>
2016-08-02 15:57:43 +0200

    new theme

--- public/src/main/webapp/public/js/controllers.js
+++ public/src/main/webapp/public/js/controllers.js
@@ -29,25 +29,29 @@ controllers
 						age : 21,
 						position : "Vedo",
 						leg : "Jobb",
-						number : 10
+						number : 10,
+						mennyeiIndex : 66
 					}, {
 						name : "Kleinheisler Laszlo",
 						age : 22,
 						position : "Kapus",
 						leg : "Jobb",
-						number : 1
+						number : 1,
+						mennyeiIndex : 34
 					}, {
 						name : "Gera Zoltan",
 						age : 23,
 						position : "Csatar",
 						leg : "Bal",
-						number : 9
+						number : 9,
+						mennyeiIndex : 99
 					}, {
 						name : "Nagy Adam",
 						age : 24,
 						position : "Kozeppalya",
 						leg : "Jobb/Bal",
-						number : 8
+						number : 8,
+						mennyeiIndex : 75
 					} ]
 					
 					$scope.clubs = [ {

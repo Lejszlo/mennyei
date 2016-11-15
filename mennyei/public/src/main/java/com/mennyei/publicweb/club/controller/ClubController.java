3facdec3c193e64bd48d2a39fff7c52425ece420
Laszlo Hajdu <Laszlo_Hajdu@mentor.com>
2016-11-15 15:46:35 +0100

    eventuate saas ids

--- mennyei/public/src/main/java/com/mennyei/publicweb/club/controller/ClubController.java
+++ mennyei/public/src/main/java/com/mennyei/publicweb/club/controller/ClubController.java
@@ -12,15 +12,12 @@ import com.mennyei.core.FillDatabase;
 import com.mennyei.core.competition.domain.Competition;
 import com.mennyei.core.team.domain.Club;
 import com.mennyei.publicweb.club.dto.PlayerMatchStatisticData;
-import com.mennyei.publicweb.club.service.ClubService;
 
 
 @RequestMapping("/clubs/")
 @RestController
 public class ClubController {
 	
-	@Autowired
-	private ClubService clubService;
 	
 	@Autowired
 	private FillDatabase fillDatabase;
@@ -31,7 +28,7 @@ public class ClubController {
 		
 		fillDatabase.fillTestMemoryDB();
 		
-		return clubService.getClubPlayersWithMatchStatisticDatas(club, competation);
+		return null;
 	}
 	
 }

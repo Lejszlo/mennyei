ad42bb60fde38f40a2ea61f270241e73f6fe6ef7
kislaci <lejszlofradi@gmail.com>
2017-02-06 00:10:51 +0100

    selected player

--- mennyei/public/src/main/java/com/mennyei/publicweb/match/infrastructure/MatchMongoRepository.java
+++ mennyei/public/src/main/java/com/mennyei/publicweb/match/infrastructure/MatchMongoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.mennyei.publicweb.match.dto.MatchQuery;
@@ -11,6 +12,6 @@ import com.mennyei.publicweb.match.dto.MatchQuery;
 @Repository
 public interface MatchMongoRepository extends MongoRepository<MatchQuery, String> {
 	@Query("{ $or: [ {'homeClub' : ?0}, {'awayClub' : ?0} ], $and: [{ 'competition.id' : ?1}] }")
-	List<MatchQuery> findClubAndCompetition(String clubId, String competitionId);
+	List<MatchQuery> findClubAndCompetition(@Param("clubId") String clubId,@Param("competitionId") String competitionId);
 
 }
--- mennyei/public/src/main/webapp/public/js/modules/teamModule.js
+++ mennyei/public/src/main/webapp/public/js/modules/teamModule.js
@@ -2,13 +2,14 @@ var teamModule = angular.module('teamModule', []);
 
 teamModule.controller('playersCtrl', function($scope, Restangular) {
 	
-	var basePlayers = Restangular.one('clubs', "00000159ffee1694-0000000000e00001").get().then(function (playersList) {
-		$scope.players = playersList.getList("players");
-		$scope.selectedPlayer = playersList[0];
-	}, function(playerList) {
-		console.println("");
-	}, function(playerList) {
-		console.println("");
+	var basePlayers = Restangular.one("clubs").customGET("search/findByUrlName", "vamosoroszikse").customGET("players").then(function (playersList) {
+		$scope.players = playersList._embedded.playerQueries;
+		$scope.selectedPlayer = $scope.players[0];
 	});
+	
+	$scope.selectPlayer = function(player) {
+		$scope.selectedPlayer = player;
+		console.log($scope.selectedPlayer.name)
+	}
 	
 });
